5ec29fefcb72c6c0a04478794071427b6ed44e0c
Stockton Rose <ros20019@byui.edu>
2025-04-30 11:32:53 -0600

    authenticatiopn program begin

--- 1. Algorithm Design/WEEK2/authentication.py
+++ 1. Algorithm Design/WEEK2/authentication.py
@@ -1,1 +1,22 @@
-print('I fart in your general direction!')
+#Lab 02
+#Authentication Program
+
+# 1. Name:
+#      -Stockton Rose-
+
+# 2. Assignment Name:
+#      Lab 02: Authentication
+
+# 3. Assignment Description:
+#      -This program reads from a json file which contains username and password
+#       information and will make sure that usernames and passwords match.-
+
+# 4. What was the hardest part? Be as specific as possible.
+#      -a paragraph or two about how the assignment went for you-
+
+# 5. How long did it take for you to complete the assignment?
+#      -total time in hours including reading the assignment and submitting the program-
+
+
+
+how do read json?
