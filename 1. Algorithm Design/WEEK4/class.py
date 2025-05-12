046b10517cb819c4c38e013f786ef3ee3b436a25
Stockton Rose <ros20019@byui.edu>
2025-05-12 12:23:05 -0600

    I took some notes and added them to the monopoly file

---
+++ 1. Algorithm Design/WEEK4/class.py
@@ -0,0 +1,2 @@
+#Class Notes for week 4
+
---
+++ 1. Algorithm Design/WEEK4/monopoly.py
@@ -0,0 +1,61 @@
+# 1. Name:
+#      -your name-
+# 2. Assignment Name:
+#      Lab 04: Monopoly
+# 3. Assignment Description:
+#      -describe what this program is meant to do-
+# 4. What was the hardest part? Be as specific as possible.
+#      Was it the syntax of Python?
+#      Was it an aspect of the problem you are to solve?
+#      Was it the instructions or any part of the problem definition?
+#      Was it the submission process?
+# 5. How long did it take for you to complete the assignment?
+#      -total time in hours including reading the assignment and submitting the program-
+
+#INPUT
+
+#Do you own all three green properties?
+#What is on Pacific Avenue?
+#What is on North Carolina?
+#What is on Pennsylvania?
+#How many hotels are available?
+#How many Houses are available?
+#How much cash do you have?
+
+
+#OUTPUT for failed cases
+
+#You cannot purchase a hotel until you own all the properties of a given color group.
+#Swap OR you don't have enough houses on this property OR move on
+#Swap OR you don't have enough houses on this property OR move on
+#Swap OR you don't have enough houses on this property OR move on
+#There are no hotels available for purchase OR move on
+#There are no houses available for purchase
+#You have insufficient funds
+
+
+#OUTPUT for successful cases
+
+#case 1
+#This will cost $[price].
+#Purchase 1 hotel and [number of houses] house(s).
+#Put 1 hotel on Pennsylvania and return any houses to the bank.
+#Put [number of houses] house(s) on North Carolina.
+#Put [number of houses] house(s) on Pacific.
+
+#case 2
+#This will cost $[price].
+#Purchase 1 hotel and [number of houses] house(s).
+#Put 1 hotel on Pennsylvania and return any houses to the bank.
+#Put [number of houses] house(s) on North Carolina.
+
+#case 3
+#This will cost $[price].
+#Purchase 1 hotel and [number of houses] house(s).
+#Put 1 hotel on Pennsylvania and return any houses to the bank.
+#Put [number of houses] house(s) on Pacific.
+
+#case 4
+#This will cost $[price].
+#Purchase 1 hotel and [number of houses] house(s).
+#Put 1 hotel on Pennsylvania and return any houses to the bank.
