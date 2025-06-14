7ea410979bad55423a0b753462788b61b3fcc4d7
Srose25 <ros20019@byui.edu>
2025-06-14 12:00:05 -0600

    final changes 2.0

--- 1. Algorithm Design/WEEK7-8/sort.py
+++ 1. Algorithm Design/WEEK7-8/sort.py
@@ -3,11 +3,23 @@
 # 2. Assignment Name:
 #      Lab 08: Sort
 # 3. Assignment Description:
-#      -describe what this program is meant to do-
+#      -This program takes a json file and is able to read it, take the data from it,
+#       and sort it from least to greatest. This could be in numbers as well as letters
+#       as the tests will demonstrate. It uses a Selection Sorting Algorithm to do this-
 # 4. What was the hardest part? Be as specific as possible.
-#      -a paragraph or two about how the assignment went for you-
+#      -At first I thought we needed to make the entire program in be done in one fell swoop.
+#       However, once we started using functions in class I just couldn't help myself. With 
+#       how big this program is I had to make it modular. The hardest part was focusing on efficiency
+#       while still having this modular design. I hope it still meets the requirements. The second
+#       hardest thing why this one was so hard is I wanted to take what we did in class and do my own
+#       thing with it. I made my own assert function to help me out in the future. I also had a hard time
+#       creating asserts that don't unintentionally trigger the assert because of the way my code is set up.-
 # 5. How long did it take for you to complete the assignment?
-#      -total time in hours including reading the assignment and submitting the program-
+#      -It took me about 3 hours creating the bulk of the code and having it be modular with the rest of the
+#       program. Part of that was also creating the my_assert() function and the test() function. Another hour
+#       was then spent on going over the code and making sure everything was optimized and double checking the
+#       way the asserts were placed and working so they can do their job right. The last 30 minutes was spent
+#       creating the video.-
 
 
 #My Updated Pseudocode
@@ -34,6 +46,8 @@ import json
 import traceback
 import inspect
 import os
+
+COUNTER1 = 0
 
 
 #Test function
@@ -74,22 +88,25 @@ def my_assert(expression, message):
 
 #Loads the json file
 def load(filename, listname):
-    my_assert(os.path.exists(filename), f'File: "{filename}" not found') #File Not Found
+    full_path = f'1. Algorithm Design/WEEK7-8/tests/{filename}' #My computer is weird ok?
+    my_assert(os.path.exists(full_path), f'File: "{full_path}" not found') #File Not Found
+
+    with open(full_path, 'r') as file:
+        data = json.load(file)
     my_assert(listname in data, f"Key '{listname}' not found in file {filename}") #List Not Found
     my_assert(isinstance(data[listname], list), f"'{listname}' must be a list") #List is a List
-
-    with open(f'1. Algorithm Design/WEEK7-8/tests/{filename}', 'r') as file:
-        data = json.load(file)
     
     return data[listname]
 
 
 #Sorting Algorithm
 def sort(info):
+    global COUNTER1 #my counter is a global variable
     my_assert(isinstance(info, list), "Input must be a list") #info is a List
-    for current_item in range(len(info)):
-        first_item = current_item
+    for current_item in range(len(info)): #Line 25 has my Pseudocode that this algorithm
+        first_item = current_item         #is based on.
         for check in range(current_item + 1, len(info)):
+            COUNTER1 += 1
             if info[check] < info[first_item]:
                 first_item = check
         if first_item != current_item:
@@ -97,27 +114,30 @@ def sort(info):
     return info
 
 
+#Prints Lists
+def write(type, data):
+    print(f'{type} List:')
+    for i, val in enumerate(data):
+        print(f'{i + 1}: {val}')
+    print()    
+
+
 #Main starts with test which can easily be commented out
 def main():
-    #test()
+    test() #These were tests I created to test my Algorithm for unexpected bugs
 
     filename = input('What is the filename? ') #Input from user
     listname = input("What is the list's name? ")
-
-    info = load(filename, listname) #Sorting
-    sorted = sort(info)
-
-    my_assert(len(info) == len(sorted), "Sorted list length mismatch") #Error in sorting
-
     print()
-    print(f'Original List: ')
-    for i, val in enumerate(info): #Print Original List
-        print(f"{i + 1}: {val}")
 
-    print()
-    print("Sorted List:")
-    for i, val in enumerate(sorted): #Print Sorted List
-        print(f"{i + 1}: {val}")
+    original = load(filename, listname) #Sorting
+    sorted = sort(original)
+
+    my_assert(len(original) == len(sorted), "Sorted list length mismatch") #Error in sorting
+
+    write('Original', original) #Print the Lists
+    write('Sorted', sorted)
+    print(f'Counter: {COUNTER1}')
 
 
 if __name__ == main():
