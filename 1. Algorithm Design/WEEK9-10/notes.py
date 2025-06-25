9916c9bf92b55eb974bc0529cbaedaa53bc23a1d
Stockton Rose <ros20019@byui.edu>
2025-06-25 12:24:23 -0600

    class examples

--- 1. Algorithm Design/WEEK9-10/notes.py
+++ 1. Algorithm Design/WEEK9-10/notes.py
@@ -3,8 +3,9 @@ def is_prime(number):
     if number < 2:
         return False
     
-    for i in range(2, math.floor(math.sqrt(number))):
-        return False if not number % i else 0
+    for i in range(2, math.floor(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
     return True
     
 
@@ -21,9 +22,16 @@ def ask_user():
 
 
 def test_prime(number):
-    pass
+    primes = []
+    
+    for i in range(2, number):
+        if is_prime(i):
+            primes.append(i)
+    print(primes)
 
 def main():
+    test_prime(100)
+
     number = ask_user()
     prime = is_prime(number)
     if prime:
@@ -31,4 +39,44 @@ def main():
     else:
         print(f'{number} is not a prime')
 
-main()
+#main()
+
+
+
+
+
+
+#loop types
+
+def simple_loop():
+    numbers = 10
+    sum = 0
+    fac = 1
+    pow = 1
+
+    for i in range(1, numbers + 1):
+        sum += i
+        fac *= i
+        pow *= 2
+
+
+    print(sum)
+    print(fac)
+    print(pow)
+
+def event_controlled():
+    pass
+
+def sentinel_loop():
+    pass
+
+
+#print triangle
+def nested_loop():
+    numbers = 10
+    count = 0
+    for count in range(0, numbers, 2):
+        print()
+        for n in range(1, count):
+            print('*', end='')
+nested_loop()
