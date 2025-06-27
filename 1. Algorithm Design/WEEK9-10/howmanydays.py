e0955ff8a82500f4446e7d33c977a7e64e7ff26d
Srose25 <ros20019@byui.edu>
2025-06-26 22:13:29 -0600

    pseudocode to python

--- 1. Algorithm Design/WEEK9-10/howmanydays.py
+++ 1. Algorithm Design/WEEK9-10/howmanydays.py
@@ -64,32 +64,54 @@ MAIN PROGRAM:
     PRINT total_days
 """
 
-def start_of_year():
-    pass
+def start_of_year(day, month, year):
+    total_days = 0
+    for n in range(1, month):
+        total_days += days_in_month(n, year)
 
-def days_in_year():
-    pass
+    total_days += day
+    return total_days
 
-def days_in_month():
-    pass
+def days_in_year(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return 366
+    else:
+        return 365
+
+def days_in_month(month, year):
+    if month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
+    elif month == 2:
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            return 29
+        else:
+            return 28
+    else:
+        return 31
 
 def main():
     print("Input the Start Date.")
-    start_day = input("Start Day: ")
-    start_month = input("Start Month: ")
-    start_year = input("Start Year: ")
+    start_day = int(input("Start Day: "))
+    start_month = int(input("Start Month: "))
+    start_year = int(input("Start Year: "))
     print()
 
     print("Input the End Date.")
-    end_day = input("End Day: ")
-    end_month = input("End Month: ")
-    end_year = input("End Year: ")
+    end_day = int(input("End Day: "))
+    end_month = int(input("End Month: "))
+    end_year = int(input("End Year: "))
     print()
 
     if start_year == end_year:
-        start_day = days_in_year(start_year) - start_of_year(start_day, start_month, start_year)
+        total_days = start_of_year(end_day, end_month, end_year) - start_of_year(start_day, start_month, start_year)
     else:
-        pass
+        total_days = days_in_year(start_year) - start_of_year(start_day, start_month, start_year)
 
-if __name__ == main:
-    main()
+        for year in range(start_year, end_year - 1):
+            total_days += days_in_year(year)
+
+    print(f'Total amound of Days: {total_days}')
+
+
+main()
+
